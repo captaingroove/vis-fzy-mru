1009f10e4190a5d7826be8cbaa88f48490e3b723
Jörg Bakker <captaingroove@openmultimedia.org>
2023-06-12 14:39:08 +0200

    Provide nil for file and range when piping to fzy

--- init.lua
+++ init.lua
@@ -20,26 +20,21 @@ end
 function write_mru(win)
 	local file_path = win.file.path
 	local mru = read_mru()
-
 	--- Check if mru data exists
 	if mru == nil then mru = {} end
 	--- Check if we opened any file
 	if file_path == nil then return end
 	--- Check duplicate
 	if file_path == mru[1] then return end
-
 	local f = io.open(module.fzymru_filepath, 'w+')
 	if f == nil then return end
-
 	table.insert(mru, 1, file_path)
-
 	for i,k in ipairs(mru) do
 		if i > module.fzymru_history then break end
 		if i == 1 or k ~= file_path then
 			f:write(string.format('%s\n', k))
 		end
 	end
-
 	f:close()
 end
 
@@ -47,21 +42,14 @@ vis.events.subscribe(vis.events.WIN_OPEN, write_mru)
 
 vis:command_register("fzymru", function(argv, force, win, selection, range)
 	local command = "cat " .. module.fzymru_filepath .. " | " .. module.fzymru_path .. " " .. module.fzymru_args .. " " .. table.concat(argv, " ")
-
 	--- Using io.popen() leads to staircase effect, the tty needs to be handled correctly
-	--- with fzy (in contrast to fzf)
-	--- TODO use nil for file parameter
-	local status, output, stderr = vis:pipe(win.file, {start = 0, finish = 0}, command)
-	-- local status, output, stderr = vis:pipe(nil, nil, command)
-
-
+	--- when using fzy (in contrast to fzf)
+	local status, output, stderr = vis:pipe(nil, nil, command)
 	if status == 0 then
 		--- Remove trailing newline
 		vis:command(string.format("e '%s'", output:sub(1, -2)))
 	end
-
 	vis:redraw()
-
 	return true;
 end)
 

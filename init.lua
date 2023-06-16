37ef01ae79c89d8d12b500bc221fdeb155bd5c2c
Jörg Bakker <captaingroove@openmultimedia.org>
2023-06-16 10:24:43 +0200

    Try piping with nil args and retreiving stderr

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ function write_mru(win)
 	if mru == nil then mru = {} end
 	--- Check if we opened any file
 	if file_path == nil then return end
-	--- Check duplicate
+	--- Check duplicates
 	if file_path == mru[1] then return end
 	local f = io.open(module.fzymru_filepath, 'w+')
 	if f == nil then return end
@@ -47,13 +47,19 @@ vis:command_register("fzymru", function(argv, force, win, selection, range)
 	local command = "cat " .. module.fzymru_filepath .. " | " .. module.fzymru_path .. " " .. module.fzymru_args .. " " .. table.concat(argv, " ")
 	--- Using io.popen() leads to staircase effect, the tty needs to be handled correctly
 	--- when using fzy (in contrast to fzf)
-	local status, output, stderr = vis:pipe(win.file, {start = 0, finish = 0}, command)
 	--- Setting file to nil currently means launching the subprocess without any piping
-	-- local status, output, stderr = vis:pipe(nil, nil, command)
+	--- so we need to provide an input file with and empty range
+	--- or otherwise a temporary file to write and read from the output of the subprocess
+	local status, output, stderr = vis:pipe(win.file, {start = 0, finish = 0}, command)
 	if status == 0 then
 		--- Remove trailing newline
 		vis:command(string.format("e '%s'", output:sub(1, -2)))
 	end
+	-- local status, output, stderr = vis:pipe(nil, nil, command)
+	-- if status == 0 then
+		-- --- Remove trailing newline
+		-- vis:command(string.format("e '%s'", stderr:sub(1, -2)))
+	-- end
 	vis:redraw()
 	return true;
 end)
